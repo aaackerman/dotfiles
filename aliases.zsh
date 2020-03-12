6463ce0caae584e34f05ec72425c8fece42ca7fc
Alexandra Ackerman <aackerman@contently.com>
2020-03-12 13:07:03 -0400

    Update aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,17 +1,250 @@
-# Custom setup
+# Aliases in this file are bash and zsh compatible
+#
+# Don't change. The following determines where YADR is installed.
+yadr=$HOME/.yadr
+
+# Get operating system
+platform='unknown'
+unamestr=$(uname)
+if [[ $unamestr == 'Linux' ]]; then
+  platform='linux'
+elif [[ $unamestr == 'Darwin' ]]; then
+  platform='darwin'
+fi
+
+# Custom
 alias be="bundle exec"
+alias dc="docker-compose"
 alias migrate="be rake db:migrate db:test:prepare"
 alias run="bin/rspec"
 alias weather="ansiweather"
-alias bootpostgres="rm /usr/local/var/postgres/postmaster.pid; brew services restart postgresql94"
+alias bootpostgres="rm /usr/local/var/postgresql@9.4/postmaster.pid; brew services restart postgresql94"
+alias cc_analyze="codeclimate analyze --dev $1"
 alias run_atd="docker run -dp 1049:1049 contently/atd"
-alias c='rails c'
+alias add_key="ssh-add -K ~/.ssh/id_rsa"
+alias add_ssh_key="ssh-add -k && ssh-add -k ~/.ssh/keys/contently-vpc.pem"
+alias kc="kubectl"
 
-# Tmux
+alias add_contently_ssh="ssh-add -k ~/.keys/contently-vpc.pem"
+
 alias tmk="tmux kill-session -t $1"
-alias tma="tmux attach =t $1"
+alias tma="tmux attach -t $1"
 alias tml="tmux ls"
 alias tmn="tmux new -s $1"
+alias tms="tmuxinator start contently"
+alias tmst="tmuxinator start toolkit"
+alias tmss="tmuxinator start strategist-storybook"
+alias tmsrg="tmuxinator start srg"
+alias tmseditor="tmuxinator start editor"
 
-# Git
+# Docker
+# Kill all running containers.
+alias dockerkillall='docker kill $(docker ps -q)'
+# # Delete all stopped containers.
+alias dockercleanc='printf "\n>>> Deleting containers\n\n" && docker rm -f $(docker ps -aq)'
+# # Delete all untagged images.
+alias dockercleani='printf "\n>>> Deleting untagged images\n\n" && docker rmi $(docker images -q)'
+# # Delete all stopped containers and untagged images.
+alias dockerclean='dockercleanc || true && dockercleani'
+
+# YADR support
+alias yav='yadr vim-add-plugin'
+alias ydv='yadr vim-delete-plugin'
+alias ylv='yadr vim-list-plugin'
+alias yup='yadr update-plugins'
+alias yip='yadr init-plugins'
+
+# PS
+alias psa="ps aux"
+alias psg="ps aux | grep "
+alias psr='ps aux | grep ruby'
+
+# Moving around
+alias cdb='cd -'
+alias cls='clear;ls'
+
+# Show human friendly numbers and colors
+alias df='df -h'
+alias du='du -h -d 2'
+
+if [[ $platform == 'linux' ]]; then
+  alias ll='ls -alh --color=auto'
+  alias ls='ls --color=auto'
+elif [[ $platform == 'darwin' ]]; then
+  alias ll='ls -alGh'
+  alias ls='ls -Gh'
+fi
+
+# show me files matching "ls grep"
+alias lsg='ll | grep'
+
+# Alias Editing
+TRAPHUP() {
+  source $yadr/zsh/aliases.zsh
+}
+
+alias ae='vim $yadr/zsh/aliases.zsh' #alias edit
+alias ar='source $yadr/zsh/aliases.zsh'  #alias reload
+alias gar="killall -HUP -u \"$USER\" zsh"  #global alias reload
+
+# vim using
+mvim --version > /dev/null 2>&1
+MACVIM_INSTALLED=$?
+if [ $MACVIM_INSTALLED -eq 0 ]; then
+  alias vim="mvim -v"
+fi
+
+# mimic vim functions
+alias :q='exit'
+
+# vimrc editing
+alias ve='vim ~/.vimrc'
+
+# zsh profile editing
+alias ze='vim ~/.zshrc'
+
+# Git Aliases
+alias gs='git status'
+alias gstsh='git stash'
+alias gst='git stash'
+alias gsp='git stash pop'
+alias gsa='git stash apply'
+alias gsh='git show'
+alias gshw='git show'
+alias gshow='git show'
+alias gi='vim .gitignore'
+alias gcm='git ci -m'
+alias gcim='git ci -m'
+alias gci='git ci'
+alias gco='git co'
+alias gcp='git cp'
+alias ga='git add -A'
+alias gap='git add -p'
+alias guns='git unstage'
+alias gunc='git uncommit'
+alias gm='git merge'
+alias gms='git merge --squash'
+alias gam='git amend --reset-author'
+alias grv='git remote -v'
+alias grr='git remote rm'
+alias grad='git remote add'
+alias gr='git rebase'
+alias gra='git rebase --abort'
+alias ggrc='git rebase --continue'
+alias gbi='git rebase --interactive'
+alias gl='git l'
+alias glg='git l'
+alias glog='git l'
+alias co='git co'
+alias gfp='git fetch --prune'
+alias gfa='git fetch --all'
+alias gfap='git fetch --all --prune'
+alias gf='git fetch --prune'
+alias gfch='git fetch'
+alias gd='git diff'
+alias gb='git b'
+# Staged and cached are the same thing
+alias gdc='git diff --cached -w'
+alias gds='git diff --staged -w'
+alias gpub='grb publish'
+alias gtr='grb track'
+alias gpl='git pull'
+alias gplr='git pull --rebase'
+alias gps='git push origin'
 alias gpf="git push --force-with-lease origin"
+alias gpsh='git push -u origin `git rev-parse --abbrev-ref HEAD`'
+alias gnb='git nb' # new branch aka checkout -b
+alias grs='git reset'
+alias grsh='git reset --hard'
+alias gcln='git clean'
+alias gclndf='git clean -df'
+alias gclndfx='git clean -dfx'
+alias gsm='git submodule'
+alias gsmi='git submodule init'
+alias gsmu='git submodule update'
+alias gt='git t'
+alias gbg='git bisect good'
+alias gbb='git bisect bad'
+alias gdmb='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
+
+# Common shell functions
+alias less='less -r'
+alias tf='tail -f'
+alias l='less'
+alias lh='ls -alt | head' # see the last modified files
+alias screen='TERM=screen screen'
+alias cl='clear'
+
+# Zippin
+alias gz='tar -zcvf'
+
+# Ruby
+alias c='rails c' # Rails 3
+alias co='script/console' # Rails 2
+alias cod='script/console --debugger'
+
+#If you want your thin to listen on a port for local VM development
+#export VM_IP=10.0.0.1 <-- your vm ip
+alias ts='thin start -a ${VM_IP:-127.0.0.1}'
+alias ms='mongrel_rails start'
+alias tfdl='tail -f log/development.log'
+alias tftl='tail -f log/test.log'
+
+alias ka9='killall -9'
+alias k9='kill -9'
+
+# Gem install
+alias sgi='sudo gem install --no-ri --no-rdoc'
+
+# TODOS
+# This uses NValt (NotationalVelocity alt fork) - http://brettterpstra.com/project/nvalt/
+# to find the note called 'todo'
+alias todo='open nvalt://find/todo'
+
+# Forward port 80 to 3000
+alias portforward='sudo ipfw add 1000 forward 127.0.0.1,3000 ip from any to any 80 in'
+
+alias rdm='rake db:migrate'
+alias rdmr='rake db:migrate:redo'
+
+# Zeus
+alias zs='zeus server'
+alias zc='zeus console'
+alias zr='zeus rspec'
+alias zrc='zeus rails c'
+alias zrs='zeus rails s'
+alias zrdbm='zeus rake db:migrate'
+alias zrdbtp='zeus rake db:test:prepare'
+alias zzz='rm .zeus.sock; pkill zeus; zeus start'
+
+# Rspec
+alias rs='rspec spec'
+alias sr='spring rspec'
+alias src='spring rails c'
+alias srgm='spring rails g migration'
+alias srdm='spring rake db:migrate'
+alias srdt='spring rake db:migrate'
+alias srdmt='spring rake db:migrate db:test:prepare'
+
+
+# Sprintly - https://github.com/nextbigsoundinc/Sprintly-GitHub
+alias sp='sprintly'
+# spb = sprintly branch - create a branch automatically based on the bug you're working on
+alias spb="git checkout -b \`sp | tail -2 | grep '#' | sed 's/^ //' | sed 's/[^A-Za-z0-9 ]//g' | sed 's/ /-/g' | cut -d"-" -f1,2,3,4,5\`"
+
+alias hpr='hub pull-request'
+alias grb='git recent-branches'
+
+# Finder
+alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+
+alias dbtp='spring rake db:test:prepare'
+alias dbm='spring rake db:migrate'
+alias dbmr='spring rake db:migrate:redo'
+alias dbmd='spring rake db:migrate:down'
+alias dbmu='spring rake db:migrate:up'
+
+# Homebrew
+alias brewu='brew update && brew upgrade && brew cleanup && brew doctor'
+alias brewuc='brew update  && brew upgrade --all && brew cleanup && brew prune && brew doctor'
